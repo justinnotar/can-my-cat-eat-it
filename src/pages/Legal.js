2a6ab9ab1a92b4cae122084c4255323f96ea69aa
Justin <justinnotar@gmail.com>
2022-11-17 21:44:58 -0500

    added legal disclosures

--- src/pages/Legal.js
+++ src/pages/Legal.js
@@ -9,11 +9,21 @@ export default function Home() {
       <TopBlur />
       <NavBar />
       <main>
-        <div className="pt-[25vh] relative mx-auto max-w-3xl h-screen">
+        <div className="relative mx-auto max-w-3xl h-screen">
             {/* <h1 className="text-5xl pb-4 font-bold text-purple-900 text-center tracking-tight"> */}
             <h1 className="text-5xl pb-4 font-bold text-purple-900 text-center">
               legal stuff
             </h1>
+            <h1 className="text-3xl text-center">website disclaimer</h1>
+            the information provided by can my cat eat xyz? ("we", "us", or "our") on canmycateat.xyz (the "Site") is for general informational purposes only. All information on the Site is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness of any information on the Site.
+            UNDER NO CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THE SITE OR RELIANCE ON ANY INFORMATION PROVIDED ON THE SITE. YOUR USE OF THE SITE AND YOUR RELIANCE ON ANY INFORMATION ON THE SITE IS SOLELY AT YOUR OWN RISK.
+            <h1 className="text-3xl text-center">external links disclaimer</h1>
+            the Site may contain (or you may be sent through the Site) links to other websites or content belonging to or origination from third parties or links to websites and features in banners or other asvertising. such external links are not investigated, monitored, or checked for accuracy, adequacy, validity, reliability, availability, or completeness by us.
+            WE DO NOT WARRANT, ENDORSE, GUARANTEE, OR ASSUME RESPONSIBILITY FOR THE ACCURACY OR RELIABILITY OF ANY INFORMATION OFFERED BY THIRD-PARTY WEBSITES LINKED THROUGH THE SITE OR ANY WEBSITE OR FEATURE LINKED IN ANY BANNER OR OTHER ADVERTISING. WE WILL NOT BE A PARTY TO OR IN ANY WAY BE RESPONSIBLE FOR MONITORING ANY TRANSACTION BETWEEN YOU AND THIRD-PARTY PROVIDERS OF PRODUCTS OR SERVICES.
+            <h1 className="text-3xl text-center">professional disclaimer</h1>
+            the Site cannot and does not contain medical/health advice. the medical/health information is provided for general informational and educational purposes only and is not a substitute for professional advice. accordingly, before taking any actions based upon such information, we encourage you to consult with the appropriate professionals. we do not provide any kind of medical/health advice.
+            THE USE OR RELIANCE OF ANY INFORMATION CONTAINED ON THE SITE IS SOLELY AT YOUR OWN RISK.
+
             <BottomBlur />
         </div>
       </main>
