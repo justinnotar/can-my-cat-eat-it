2ec39fe082394fafbf63548fe38c70a6a319aecb
Justin <justinnotar@gmail.com>
2022-11-22 21:59:13 -0500

    WIP: submit button needs work

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,8 +1,5 @@
 import '../index.css'
-import { useState } from 'react'
-import { pages } from '../assets/content'
 import { Combobox } from '@headlessui/react'
-import { SearchIcon } from '@heroicons/react/outline'
 
 export default function SubmitForm() {
     
@@ -41,37 +38,29 @@ export default function SubmitForm() {
                         <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                             reason
                         </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="peanut butter too sticky for little cat mouths" />
-                        <p class="text-gray-600 text-xs italic">make it as long and as crazy as you'd like</p>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="peanut butter is too sticky for little cat mouths" />
+                        {/* <p class="text-gray-600 text-xs italic">make it as long and as crazy as you'd like</p> */}
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                        <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                            source url
+                        </label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="peanut butter is too sticky for little cat mouths" />
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-2">
                         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                        <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                            City
-                        </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
-                        </div>
-                        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                            <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                                can your cat eat it?
+                            <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-name">
+                                name
                             </label>
-                            <div class="relative">
-                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                <option>yes</option>
-                                <option>no</option>
-                                <option>maybe</option>
-                                </select>
-                                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                                </div>
-                            </div>
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-name" type="text" placeholder="justin" />
                         </div>
-                        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                        <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                            Zip
-                        </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
+                        <div class="w-full md:w-2/3 px-3 md:mb-0">
+                            <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                            submit
+                            </button>
                         </div>
                     </div>
                 </form>
