b59453c88759521d1112852ae8954c5cbd50c21d
Justin <justinnotar@gmail.com>
2022-11-24 00:42:15 -0500

    even more changes to submit form

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -22,10 +22,10 @@ export default function SubmitForm() {
                                 can your cat eat it?
                             </label>
                             <div class="relative">
-                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-800 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-state">
-                                <option>yes</option>
-                                <option>no</option>
-                                <option>maybe</option>
+                                <select class="required block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-800 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-state">
+                                    <option>yes</option>
+                                    <option>no</option>
+                                    <option>maybe</option>
                                 </select>
                                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-800">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -35,10 +35,10 @@ export default function SubmitForm() {
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3">
-                        <label class="block  tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-password">
+                        <label class="block tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-password">
                             reason
                         </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-password" type="text" placeholder="peanut butter is too sticky for little cat mouths" />
+                        <input class="required appearance-none block w-full bg-gray-200 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-password" type="text" placeholder="peanut butter is too sticky for their little cat mouths" />
                         {/* <p class="text-gray-600 text-xs italic">make it as long and as crazy as you'd like</p> */}
                         </div>
                     </div>
@@ -47,16 +47,18 @@ export default function SubmitForm() {
                         <label class="block  tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-password">
                             source url
                         </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-password" type="text" placeholder="peanut butter is too sticky for little cat mouths" />
+                        <input class="required appearance-none block w-full bg-gray-200 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-password" type="text" placeholder="www.totallyreliablesource.com" />
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-2">
                         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label class="block  tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-name">
+                            <label class="block tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-name">
                                 name
                             </label>
                             <input class="appearance-none block w-full bg-gray-200 text-gray-800 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-name" type="text" placeholder="justin" />
                         </div>
+
+                        {/* TODO: fix submit button size on half screen */}
                         <div class="md:w-1/2 px-3 md:mb-0">
                             <button class="w-full shadow bg-purple-900 hover:bg-purple-300 focus:shadow-outline focus:outline-none text-white font-bold py-3 mt-6 rounded" type="button">
                             submit
