93f595aa9acef820acffce32f17a341e0a408380
Justin <justinnotar@gmail.com>
2022-12-08 20:04:52 -0500

    fixed console warnings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,17 +28,17 @@ export default function NavBar() {
           >
             <span className="sr-only">open main menu</span>
             <svg
-              class="h-6 w-6"
+              className="h-6 w-6"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               />
             </svg>
--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -5,11 +5,11 @@ import { Combobox } from "@headlessui/react";
 // import database from '../firebase'
 
 export default function SubmitForm() {
-  const [name, setName] = useState(null);
-  const [answer, setAnswer] = useState(null);
-  const [reason, setReason] = useState(null);
-  const [source, setSource] = useState(null);
-  const [author, setAuthor] = useState(null);
+  const [name, setName] = useState();
+  const [answer, setAnswer] = useState();
+  const [reason, setReason] = useState();
+  const [source, setSource] = useState();
+  const [author, setAuthor] = useState();
   const handleSubmit = () => {
     console.log(name, answer, reason, source, author);
     // set(ref(database, '/test' + author), {
@@ -24,54 +24,54 @@ export default function SubmitForm() {
   return (
     <Combobox as="div" className="overflow-y-auto p-10 pt-4 pb-20">
       <div className="mx-auto max-w-xl divide-y divide-purple-300 rounded-xl bg-white p-4 text-xl text-purple-900 shadow-2xl ring-1 ring-black/5">
-        <form class="w-full max-w-xl">
-          <div class="-mx-3 flex flex-wrap">
-            <div class="w-full px-3 md:mb-0 md:w-2/3">
+        <form className="w-full max-w-xl">
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3 md:mb-0 md:w-2/3">
               <label
-                class="mb-2 block text-xs font-bold tracking-wide text-gray-800"
-                for="name"
+                className="mb-2 block text-xs font-bold tracking-wide text-gray-800"
+                htmlFor="name"
               >
                 food item
               </label>
               <input
                 required
-                class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
+                className="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
                 id="name"
                 type="text"
                 placeholder="peanut butter"
                 name="name"
                 onChange={(e) => setName(e.target.value)}
-                value={name}
+                value={name || ''}
               />
-              {/* <p class="text-red-500 text-xs italic">Please fill out this field.</p> */}
+              {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
             </div>
-            <div class="w-full px-3 md:mb-0 md:w-1/3">
+            <div className="w-full px-3 md:mb-0 md:w-1/3">
               <label
-                class="mb-2 block text-xs font-bold tracking-wide text-gray-800"
-                for="answer"
+                className="mb-2 block text-xs font-bold tracking-wide text-gray-800"
+                htmlFor="answer"
               >
                 can your cat eat it?
               </label>
-              <div class="relative">
+              <div className="relative">
                 <select
                   required
-                  class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 pr-8 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
+                  className="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 pr-8 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
                   id="answer"
                   type="select"
                   name="answer"
                   onChange={(e) => setAnswer(e.target.value)}
-                  value={answer}
+                  value={answer || ''}
                 >
-                  <option value="" disabled selected>
+                  <option value="" defaultValue="">
                     select
                   </option>
                   <option>yes</option>
                   <option>no</option>
                   <option>maybe</option>
                 </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-800">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-800">
                   <svg
-                    class="h-4 w-4 fill-current"
+                    className="h-4 w-4 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -81,72 +81,72 @@ export default function SubmitForm() {
               </div>
             </div>
           </div>
-          <div class="-mx-3 flex flex-wrap">
-            <div class="w-full px-3">
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3">
               <label
-                class="mb-2 block text-xs font-bold tracking-wide text-gray-800"
-                for="reason"
+                className="mb-2 block text-xs font-bold tracking-wide text-gray-800"
+                htmlFor="reason"
               >
                 reason
               </label>
               <input
                 required
-                class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
+                className="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
                 id="reason"
                 type="text"
                 placeholder="peanut butter is too sticky for their little cat mouths"
                 name="reason"
                 onChange={(e) => setReason(e.target.value)}
-                value={reason}
+                value={reason || ''}
               />
-              {/* <p class="text-gray-600 text-xs italic">make it as long and as crazy as you'd like</p> */}
+              {/* <p className="text-gray-600 text-xs italic">make it as long and as crazy as you'd like</p> */}
             </div>
           </div>
-          <div class="-mx-3 flex flex-wrap">
-            <div class="w-full px-3">
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3">
               <label
-                class="mb-2  block text-xs font-bold tracking-wide text-gray-800"
-                for="source"
+                className="mb-2  block text-xs font-bold tracking-wide text-gray-800"
+                htmlFor="source"
               >
                 source url
               </label>
               <input
                 required
-                class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
+                className="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
                 id="source"
                 type="text"
                 placeholder="www.totallyreliablesource.com"
                 name="source"
                 onChange={(e) => setSource(e.target.value)}
-                value={source}
+                value={source || ''}
               />
             </div>
           </div>
-          <div class="-mx-3 mb-2 flex flex-wrap">
-            <div class="w-full px-3 md:mb-0  md:w-1/2">
+          <div className="-mx-3 mb-2 flex flex-wrap">
+            <div className="w-full px-3 md:mb-0  md:w-1/2">
               <label
-                class="mb-2 block text-xs font-bold tracking-wide text-gray-800"
-                for="author"
+                className="mb-2 block text-xs font-bold tracking-wide text-gray-800"
+                htmlFor="author"
               >
                 name (optional)
               </label>
               <input
-                class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
+                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-800 focus:border-gray-400 focus:bg-white focus:outline-none"
                 id="author"
                 type="text"
                 placeholder="justin"
                 name="author"
                 onChange={(e) => setAuthor(e.target.value)}
-                value={author}
+                value={author || ''}
               />
             </div>
             {/* TODO: make button functional with form */}
             {/* Integrate with Google Sheets to populate data */}
             {/* https://github.com/levinunnink/html-form-to-google-sheet */}
             {/* https://lovespreadsheets.medium.com/save-web-html-form-data-to-google-sheets-47e48f7517e6 */}
-            <div class="w-full px-3 md:mb-0 md:w-1/2">
+            <div className="w-full px-3 md:mb-0 md:w-1/2">
               <input
-                class="focus:shadow-outline mt-6 w-full cursor-pointer rounded bg-purple-900 py-3 font-bold text-white shadow hover:bg-purple-500 focus:outline-none"
+                className="focus:shadow-outline mt-6 w-full cursor-pointer rounded bg-purple-900 py-3 font-bold text-white shadow hover:bg-purple-500 focus:outline-none"
                 type="submit"
                 value="submit"
                 onClick={() => handleSubmit()}
